66a5c9f06ccb8a590f5025177e59c645b7005ab4
kanghyun98 <lkh981107@gmail.com>
2022-07-20 13:33:24 +0900

    solve: 22/07/20

--- questions/00015-medium-last/test-cases.ts
+++ questions/00015-medium-last/test-cases.ts
@@ -1,6 +1,13 @@
-import type { Equal, Expect } from '@type-challenges/utils'
+import type { Equal, Expect } from '@type-challenges/utils';
+
+type Last<T extends any[]> = T extends [...any, infer L] ? L : never;
 
 type cases = [
   Expect<Equal<Last<[3, 2, 1]>, 1>>,
   Expect<Equal<Last<[() => 123, { a: string }]>, { a: string }>>,
-]
+];
+
+/*
+*해설
+rest property를 사용하여 배열 T의 마지막 요소를 L로 두고, L의 타입을 런타임 환경에서 추론하여 반환
+*/
--- questions/00016-medium-pop/test-cases.ts
+++ questions/00016-medium-pop/test-cases.ts
@@ -1,6 +1,13 @@
-import type { Equal, Expect } from '@type-challenges/utils'
+import type { Equal, Expect } from '@type-challenges/utils';
+
+type Pop<T extends any[]> = T extends [...infer B, infer L] ? B : never;
 
 type cases = [
   Expect<Equal<Pop<[3, 2, 1]>, [3, 2]>>,
-  Expect<Equal<Pop<['a', 'b', 'c', 'd' ]>, ['a', 'b', 'c']>>,
-]
+  Expect<Equal<Pop<['a', 'b', 'c', 'd']>, ['a', 'b', 'c']>>,
+];
+
+/*
+*해설
+rest property를 사용하여 배열 T의 마지막 요소를 L, 나머지는 B로 두고, B의 타입을 런타임 환경에서 추론하여 반환
+*/
