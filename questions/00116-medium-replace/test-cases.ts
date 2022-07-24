b58d6e3505579b6c28415ec571519192cbccc94e
kanghyun98 <lkh981107@gmail.com>
2022-07-24 16:39:37 +0900

    solve: 22/07/24

--- questions/00116-medium-replace/test-cases.ts
+++ questions/00116-medium-replace/test-cases.ts
@@ -1,4 +1,10 @@
-import type { Equal, Expect } from '@type-challenges/utils'
+import type { Equal, Expect } from '@type-challenges/utils';
+
+type Replace<
+  S extends string,
+  F extends string,
+  T extends string,
+> = S extends `${infer A}${F}${infer B}` ? (F extends '' ? S : `${A}${T}${B}`) : S;
 
 type cases = [
   Expect<Equal<Replace<'foobar', 'bar', 'foo'>, 'foofoo'>>,
@@ -7,4 +13,11 @@ type cases = [
   Expect<Equal<Replace<'foobarbar', 'bar', ''>, 'foobar'>>,
   Expect<Equal<Replace<'foobarbar', 'bra', 'foo'>, 'foobarbar'>>,
   Expect<Equal<Replace<'', '', ''>, ''>>,
-]
+];
+
+/*
+*해설
+기존 문자열: S, 변경될 문자열: F, 변경할 문자열: T
+문자열 S를 `${infer A}${F}${infer B}`로 두어 F -> T로 교체될 수 있도록 만들었다.
+F가 빈문자열이거나 F를 찾을 수 없을 경우, 기존 문자열 S가 그대로 나올 수 있도록 만들었다.
+*/
--- questions/00119-medium-replaceall/test-cases.ts
+++ questions/00119-medium-replaceall/test-cases.ts
@@ -1,4 +1,16 @@
-import type { Equal, Expect } from '@type-challenges/utils'
+import type { Equal, Expect } from '@type-challenges/utils';
+
+type ReplaceAll<
+  S extends string,
+  F extends string,
+  T extends string,
+> = S extends `${infer A}${F}${infer B}`
+  ? F extends ''
+    ? S
+    : `${ReplaceAll<A, F, T>}${T}${ReplaceAll<B, F, T>}`
+  : S;
+
+type test = ReplaceAll<'foobarfoobar', 'ob', 'b'>; // 'fobarfobar'
 
 type cases = [
   Expect<Equal<ReplaceAll<'foobar', 'bar', 'foo'>, 'foofoo'>>,
@@ -10,4 +22,13 @@ type cases = [
   Expect<Equal<ReplaceAll<'foobarfoobar', 'ob', 'b'>, 'fobarfobar'>>,
   Expect<Equal<ReplaceAll<'foboorfoboar', 'bo', 'b'>, 'foborfobar'>>,
   Expect<Equal<ReplaceAll<'', '', ''>, ''>>,
-]
+];
+
+/*
+*해설
+116번 replace 문제와 비슷하게 해결하였으나, replaceAll인 점을 고려하여 재귀적으로 수행될 수 있게 만들었다.
+
+replace에서는 문자열 S를 `${infer A}${F}${infer B}`로 두어 F -> T로 교체될 수 있도록 만들은 반면,
+replaceAll에서는 `${infer A}${F}${infer B}`의 F를 T로 대체하는 부분은 동일하나, A와 B에 각각 ReplaceAll을 재귀적으로 적용하여 
+모든 F에 대해 replace가 적용될 수 있도록 만들었다.
+*/
