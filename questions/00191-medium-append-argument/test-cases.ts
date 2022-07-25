1a46f47fe90fc3c63475e659171d9133a69a1772
kanghyun98 <lkh981107@gmail.com>
2022-07-25 16:02:13 +0900

    solve: 22/07/25

--- questions/00191-medium-append-argument/test-cases.ts
+++ questions/00191-medium-append-argument/test-cases.ts
@@ -1,12 +1,19 @@
-import type { Equal, Expect } from '@type-challenges/utils'
+import type { Equal, Expect } from '@type-challenges/utils';
 
-type Case1 = AppendArgument<(a: number, b: string) => number, boolean>
-type Result1 = (a: number, b: string, x: boolean) => number
+type AppendArgument<F, T> = F extends (...args: infer A) => infer R
+  ? (...args: [...A, T]) => R
+  : never;
 
-type Case2 = AppendArgument<() => void, undefined>
-type Result2 = (x: undefined) => void
+type Case1 = AppendArgument<(a: number, b: string) => number, boolean>;
+type Result1 = (a: number, b: string, x: boolean) => number;
 
-type cases = [
-  Expect<Equal<Case1, Result1>>,
-  Expect<Equal<Case2, Result2>>,
-]
+type Case2 = AppendArgument<() => void, undefined>;
+type Result2 = (x: undefined) => void;
+
+type cases = [Expect<Equal<Case1, Result1>>, Expect<Equal<Case2, Result2>>];
+
+/*
+*해설
+함수 F에 대해서 F extends (...args: infer A) => infer R 라면,
+추가하고자 하는 타입 T를 args의 타입에 추가해주면 되므로 (...args: [...A, T]) => R 타입을 반환한다
+*/
--- questions/00296-medium-permutation/test-cases.ts
+++ questions/00296-medium-permutation/test-cases.ts
@@ -1,8 +1,42 @@
-import type { Equal, Expect } from '@type-challenges/utils'
+import type { Equal, Expect } from '@type-challenges/utils';
+
+type Permutation<T, U = T> = [T] extends [never]
+  ? []
+  : T extends U
+  ? [T, ...Permutation<Exclude<U, T>>]
+  : [];
+
+type test = Permutation<'A' | 'B' | 'C'>;
 
 type cases = [
   Expect<Equal<Permutation<'A'>, ['A']>>,
-  Expect<Equal<Permutation<'A' | 'B' | 'C'>, ['A', 'B', 'C'] | ['A', 'C', 'B'] | ['B', 'A', 'C'] | ['B', 'C', 'A'] | ['C', 'A', 'B'] | ['C', 'B', 'A']>>,
-  Expect<Equal<Permutation<'B' | 'A' | 'C'>, ['A', 'B', 'C'] | ['A', 'C', 'B'] | ['B', 'A', 'C'] | ['B', 'C', 'A'] | ['C', 'A', 'B'] | ['C', 'B', 'A']>>,
+  Expect<
+    Equal<
+      Permutation<'A' | 'B' | 'C'>,
+      | ['A', 'B', 'C']
+      | ['A', 'C', 'B']
+      | ['B', 'A', 'C']
+      | ['B', 'C', 'A']
+      | ['C', 'A', 'B']
+      | ['C', 'B', 'A']
+    >
+  >,
+  Expect<
+    Equal<
+      Permutation<'B' | 'A' | 'C'>,
+      | ['A', 'B', 'C']
+      | ['A', 'C', 'B']
+      | ['B', 'A', 'C']
+      | ['B', 'C', 'A']
+      | ['C', 'A', 'B']
+      | ['C', 'B', 'A']
+    >
+  >,
   Expect<Equal<Permutation<never>, []>>,
-]
+];
+
+/*
+*해설
+우선 never이 들어왔을 때, 빈 배열 []을 반환하기 위한 처리를 먼저 진행하고,
+T에 A, B, C가 순차적으로 들어가며, 해당 요소를 제거한 타입들에 대해 재귀적으로 순열을 구하는 방식이다.
+*/
