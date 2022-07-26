6a5261e0435bc98f5aed50acc6a84cc04b9ead2d
kanghyun98 <lkh981107@gmail.com>
2022-07-26 16:43:41 +0900

    solve: 22/07/26

--- questions/00298-medium-length-of-string/test-cases.ts
+++ questions/00298-medium-length-of-string/test-cases.ts
@@ -1,8 +1,21 @@
-import type { Equal, Expect } from '@type-challenges/utils'
+import type { Equal, Expect } from '@type-challenges/utils';
+
+type LengthOfString<T extends string, L extends string[] = []> = T extends `${infer S}${infer R}`
+  ? LengthOfString<R, [...L, R]>
+  : L['length'];
 
 type cases = [
   Expect<Equal<LengthOfString<''>, 0>>,
   Expect<Equal<LengthOfString<'kumiko'>, 6>>,
   Expect<Equal<LengthOfString<'reina'>, 5>>,
   Expect<Equal<LengthOfString<'Sound! Euphonium'>, 16>>,
-]
+];
+
+/*
+*해설
+타입에선 그냥 문자열의 길이를 추출할 수 없으므로, 문자열의 각 요소를 배열의 요소로 만든 후 배열의 길이를 파악하는 방식을 사용한다.
+문자열 T와 문자열의 요소를 저장할 배열 L을 generic으로 받고
+문자열 T가 `${infer S}${infer R}`의 확장자라면, 
+재귀적으로 맨앞 문자열 S를 제외한 새로운 문자열 R과 기존 배열 L과 문자열 R을 포함한 배열을 generic으로 넘긴다.
+최종적으로 빈 문자열이 된다면, 배열 L의 길이를 파악하여 결과를 반환한다.
+*/
--- questions/00459-medium-flatten/test-cases.ts
+++ questions/00459-medium-flatten/test-cases.ts
@@ -1,4 +1,10 @@
-import type { Equal, Expect } from '@type-challenges/utils'
+import type { Equal, Expect } from '@type-challenges/utils';
+
+type Flatten<T extends unknown[]> = T extends [infer F, ...infer R]
+  ? F extends unknown[]
+    ? [...Flatten<F>, ...Flatten<R>]
+    : [F, ...Flatten<R>]
+  : [];
 
 type cases = [
   Expect<Equal<Flatten<[]>, []>>,
@@ -6,4 +12,11 @@ type cases = [
   Expect<Equal<Flatten<[1, [2]]>, [1, 2]>>,
   Expect<Equal<Flatten<[1, 2, [3, 4], [[[5]]]]>, [1, 2, 3, 4, 5]>>,
   Expect<Equal<Flatten<[{ foo: 'bar'; 2: 10 }, 'foobar']>, [{ foo: 'bar'; 2: 10 }, 'foobar']>>,
-]
+];
+
+/*
+*해설
+배열 T가 [infer F, ...infer R] 확장자가 아니면, 즉 빈 배열이면 빈 배열이 반환되도록 만들고,
+만약 맞다면, R에 Flatten을 적용하고
+F가 배열 내부의 배열일 수 있으므로, F가 배열인지도 검사하여 Flatten을 적용할 수 있도록 한다.
+*/
