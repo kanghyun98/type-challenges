7d7c1cb98cbe338387cc42af177ef73b25974da8
kanghyun98 <lkh981107@gmail.com>
2022-07-27 16:27:36 +0900

    solve: 22/07/27

--- questions/00527-medium-append-to-object/test-cases.ts
+++ questions/00527-medium-append-to-object/test-cases.ts
@@ -1,44 +1,56 @@
-import type { Equal, Expect } from '@type-challenges/utils'
+import type { Equal, Expect } from '@type-challenges/utils';
 
 type test1 = {
-  key: 'cat'
-  value: 'green'
-}
+  key: 'cat';
+  value: 'green';
+};
 
 type testExpect1 = {
-  key: 'cat'
-  value: 'green'
-  home: boolean
-}
+  key: 'cat';
+  value: 'green';
+  home: boolean;
+};
 
 type test2 = {
-  key: 'dog' | undefined
-  value: 'white'
-  sun: true
-}
+  key: 'dog' | undefined;
+  value: 'white';
+  sun: true;
+};
 
 type testExpect2 = {
-  key: 'dog' | undefined
-  value: 'white'
-  sun: true
-  home: 1
-}
+  key: 'dog' | undefined;
+  value: 'white';
+  sun: true;
+  home: 1;
+};
 
 type test3 = {
-  key: 'cow'
-  value: 'yellow'
-  sun: false
-}
+  key: 'cow';
+  value: 'yellow';
+  sun: false;
+};
 
 type testExpect3 = {
-  key: 'cow'
-  value: 'yellow'
-  sun: false
-  isMotherRussia: false | undefined
-}
+  key: 'cow';
+  value: 'yellow';
+  sun: false;
+  isMotherRussia: false | undefined;
+};
+
+type AppendToObject<O, K extends string, V> = {
+  [T in keyof O | K]: T extends keyof O ? O[T] : V;
+};
 
 type cases = [
   Expect<Equal<AppendToObject<test1, 'home', boolean>, testExpect1>>,
   Expect<Equal<AppendToObject<test2, 'home', 1>, testExpect2>>,
   Expect<Equal<AppendToObject<test3, 'isMotherRussia', false | undefined>, testExpect3>>,
-]
+];
+
+/*
+*해설
+generic 설명) 기존 객체: O, 새로 들어올 key: K, 새로 들어올 value: V
+객체 O의 키 값들과 새로 들어올 키 값인 K를 union type으로 묶은 뒤,
+in 연산자를 사용해 순회하며, 
+기존 객체 O에 있는 값이면 기존 값 그대로, 아니면 V 값을 반환하도록 만들어 새로운 객체를 생성한다.
+*/
--- questions/00529-medium-absolute/test-cases.ts
+++ questions/00529-medium-absolute/test-cases.ts
@@ -1,4 +1,6 @@
-import type { Equal, Expect } from '@type-challenges/utils'
+import type { Equal, Expect } from '@type-challenges/utils';
+
+type Absolute<T extends number | string | bigint> = `${T}` extends `-${infer N}` ? N : `${T}`;
 
 type cases = [
   Expect<Equal<Absolute<0>, '0'>>,
@@ -11,4 +13,11 @@ type cases = [
   Expect<Equal<Absolute<'-5'>, '5'>>,
   Expect<Equal<Absolute<-1_000_000n>, '1000000'>>,
   Expect<Equal<Absolute<9_999n>, '9999'>>,
-]
+];
+
+/*
+*해설
+숫자값 자체를 절댓값을 구하는 것은 불가능하므로,
+숫자를 문자열로 변환 후, "-" 문자가 앞에 붙어 있다면 "-"를 제외한 문자열을, 없다면 그대로를 반환할 수 있도록 만든다.
+
+*/
