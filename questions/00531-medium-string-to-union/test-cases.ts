1e5c036abaf4a19b06d7bd2dd7cd3d33eaa32eaa
kanghyun98 <lkh981107@gmail.com>
2022-07-31 15:23:28 +0900

    solve: 22/07/31

--- questions/00531-medium-string-to-union/test-cases.ts
+++ questions/00531-medium-string-to-union/test-cases.ts
@@ -1,8 +1,23 @@
-import type { Equal, Expect } from '@type-challenges/utils'
+import type { Equal, Expect } from '@type-challenges/utils';
+
+type StringToUnion<T extends string> = T extends `${infer S}${infer R}`
+  ? S | StringToUnion<R>
+  : never;
 
 type cases = [
   Expect<Equal<StringToUnion<''>, never>>,
   Expect<Equal<StringToUnion<'t'>, 't'>>,
   Expect<Equal<StringToUnion<'hello'>, 'h' | 'e' | 'l' | 'l' | 'o'>>,
-  Expect<Equal<StringToUnion<'coronavirus'>, 'c' | 'o' | 'r' | 'o' | 'n' | 'a' | 'v' | 'i' | 'r' | 'u' | 's'>>,
-]
+  Expect<
+    Equal<
+      StringToUnion<'coronavirus'>,
+      'c' | 'o' | 'r' | 'o' | 'n' | 'a' | 'v' | 'i' | 'r' | 'u' | 's'
+    >
+  >,
+];
+
+/*
+*해설
+문자열 T를 `${infer S}${infer R}`로 두어 한 문자와 나머지 문자열에 대해 union type이 적용될 수 있도록 만들고 
+재귀적으로 이를 적용하여 문자열 T의 모든 문자에 대해 union type이 적용될 수 있도록 만듦
+*/
--- questions/00599-medium-merge/test-cases.ts
+++ questions/00599-medium-merge/test-cases.ts
@@ -1,18 +1,34 @@
-import type { Equal, Expect } from '@type-challenges/utils'
+import type { Equal, Expect } from '@type-challenges/utils';
+
+type Merge<A, B> = {
+  [K in keyof A | keyof B]: K extends keyof B ? B[K] : K extends keyof A ? A[K] : never;
+};
 
 type Foo = {
-  a: number
-  b: string
-}
+  a: number;
+  b: string;
+};
 type Bar = {
-  b: number
-  c: boolean
-}
+  b: number;
+  c: boolean;
+};
 
 type cases = [
-  Expect<Equal<Merge<Foo, Bar>, {
-    a: number
-    b: number
-    c: boolean
-  }>>,
-]
+  Expect<
+    Equal<
+      Merge<Foo, Bar>,
+      {
+        a: number;
+        b: number;
+        c: boolean;
+      }
+    >
+  >,
+];
+
+/*
+*해설
+객체 A와 B의 결합된 타입을 제공하는 Merge를 만들기 위해 A와 B의 key 값들(union type)을 순회(with 'in' operator)한다.
+조건에서 B의 키가 A의 키를 override 한다고 했으므로, 키 값 K가 B의 키 값인지 먼저 확인 후 해당하는 키 값을 적용하도록 만들었다.
+
+*/
