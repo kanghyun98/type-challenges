a6a4e26690c5b73bfb0223879932365454b1ba75
kanghyun98 <lkh981107@gmail.com>
2022-08-01 16:59:23 +0900

    solve: 22/08/01

--- questions/00612-medium-kebabcase/test-cases.ts
+++ questions/00612-medium-kebabcase/test-cases.ts
@@ -1,4 +1,10 @@
-import type { Equal, Expect } from '@type-challenges/utils'
+import type { Equal, Expect } from '@type-challenges/utils';
+
+type KebabCase<T extends string> = T extends `${infer S}${infer R}`
+  ? R extends Uncapitalize<R>
+    ? `${Uncapitalize<S>}${KebabCase<R>}`
+    : `${Uncapitalize<S>}-${KebabCase<Uncapitalize<R>>}`
+  : T;
 
 type cases = [
   Expect<Equal<KebabCase<'FooBarBaz'>, 'foo-bar-baz'>>,
@@ -10,4 +16,14 @@ type cases = [
   Expect<Equal<KebabCase<'-'>, '-'>>,
   Expect<Equal<KebabCase<''>, ''>>,
   Expect<Equal<KebabCase<'😎'>, '😎'>>,
-]
+];
+
+/*
+*해설
+@Uncapitalize: Converts the first character in the string to a lowercase equivalent.
+
+문자열 T를 `${infer S}${infer R}`로 두고,
+첫 문자인 S에 대해서는 무조건 소문자가 될 수 있게 Uncapitalize<S>를 적용하고,
+나머지 문자열 R에 대해서는 맨 앞 문자가 소문자면 KebabCase를 재귀적으로 적용, 
+아니라면 "-"를 붙여주고 Uncapitalize<R>한 문자열에 대해 KebabCase를 재귀적으로 적용한다.
+*/
--- questions/00645-medium-diff/test-cases.ts
+++ questions/00645-medium-diff/test-cases.ts
@@ -1,22 +1,36 @@
-import type { Equal, Expect } from '@type-challenges/utils'
+import type { Equal, Expect } from '@type-challenges/utils';
+
+type Diff<A, B> = {
+  [K in Exclude<keyof (A & B), keyof (A | B)>]: K extends keyof A
+    ? A[K]
+    : K extends keyof B
+    ? B[K]
+    : never;
+};
 
 type Foo = {
-  name: string
-  age: string
-}
+  name: string;
+  age: string;
+};
 type Bar = {
-  name: string
-  age: string
-  gender: number
-}
+  name: string;
+  age: string;
+  gender: number;
+};
 type Coo = {
-  name: string
-  gender: number
-}
+  name: string;
+  gender: number;
+};
 
 type cases = [
   Expect<Equal<Diff<Foo, Bar>, { gender: number }>>,
   Expect<Equal<Diff<Bar, Foo>, { gender: number }>>,
   Expect<Equal<Diff<Foo, Coo>, { age: string; gender: number }>>,
   Expect<Equal<Diff<Coo, Foo>, { age: string; gender: number }>>,
-]
+];
+
+/*
+*해설
+(합집합 - 교집합) 부분에 대한 타입을 원하므로 Exclude 타입을 적용하여 
+해당 키 값들에 대해서만 순회를 돌 수 있게 만들었다.
+*/
