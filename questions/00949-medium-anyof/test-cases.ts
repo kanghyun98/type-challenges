a6f03e3c22c54795988059930d1f862fe46f8be9
kanghyun98 <lkh981107@gmail.com>
2022-08-03 16:59:28 +0900

    solve: 22/08/03

--- questions/00949-medium-anyof/test-cases.ts
+++ questions/00949-medium-anyof/test-cases.ts
@@ -1,4 +1,10 @@
-import type { Equal, Expect } from '@type-challenges/utils'
+import type { Equal, Expect } from '@type-challenges/utils';
+
+type AnyOf<T extends unknown[]> = T extends [infer S, ...infer R]
+  ? S extends 0 | '' | false | [] | Record<string, never>
+    ? AnyOf<R>
+    : true
+  : false;
 
 type cases = [
   Expect<Equal<AnyOf<[1, 'test', true, [1], { name: 'test' }, { 1: 'test' }]>, true>>,
@@ -11,4 +17,18 @@ type cases = [
   Expect<Equal<AnyOf<[0, '', false, [], { name: 'test' }, { 1: 'test' }]>, true>>,
   Expect<Equal<AnyOf<[0, '', false, [], {}]>, false>>,
   Expect<Equal<AnyOf<[]>, false>>,
-]
+];
+
+/*
+*해설
+배열 요소 중 하나라도 true 값이면 true를 반환, 아니면 false를 반환하는 AnyOf 타입의 구현
+(js에서 빈 배열, 빈 객체가 true로 평가되지만, 문제에서 이 또한 false로 반환할 수 있도록 구현하라고 함)
+
+배열을 순회할 수 있게, 배열 T를 "T extends [infer S, ...infer R]" 로 두고, S 요소에 대해 true or false 를 판단할 수 있도록 만들었다.
+true or false를 판단하는 기준은 Union Type을 이용해 ( 0 | '' | false | [] | Record<string, never> ) 을 사용하였다.
+
+주의할 점)
+빈 객체인 {}는 Object 타입과 동일하게 동작하므로, 이를 Union Type의 요소로 두게 되면
+빈 객체와 배열 뿐만 아니라 값이 들어있는 객체와 배열, number, string, boolean에 해당하는 값들이 true로 반환된다.
+(참고자료: https://stackoverflow.com/questions/49464634/difference-between-object-and-object-in-typescript)
+*/
--- questions/01042-medium-isnever/test-cases.ts
+++ questions/01042-medium-isnever/test-cases.ts
@@ -1,4 +1,7 @@
-import type { Equal, Expect } from '@type-challenges/utils'
+import type { Equal, Expect } from '@type-challenges/utils';
+
+// type IsNever<T> = T extends never ? true : false;
+type IsNever<T> = [T] extends [never] ? true : false;
 
 type cases = [
   Expect<Equal<IsNever<never>, true>>,
@@ -8,4 +11,11 @@ type cases = [
   Expect<Equal<IsNever<null>, false>>,
   Expect<Equal<IsNever<[]>, false>>,
   Expect<Equal<IsNever<{}>, false>>,
-]
+];
+
+/*
+*해설
+타입 T가 never 타입일 때만 true를 반환하는 IsNever을 구현하기 위해,
+처음에는 "T extends never"를 사용하려 했으나, 이는 T 값이 존재하지 않는 경우에만 true로 반환되기 때문에 무조건 false가 반환된다.
+그래서 T 타입 자체가 never 타입인지 확인하기 위해 "[T] extends [never]"를 사용하여 해결하였다.
+*/
